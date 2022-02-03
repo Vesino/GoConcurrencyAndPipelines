d3b5dc534d1707effce9bb4906cb4b0a1f8feb13
Luis Vargas <luisvargasr10@gmail.com>
2022-02-02 21:49:00 -0600

    Multiplexacion using select statement

---
+++ selecCase/mp.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// When a routine is communicating with several channels
+// is very useful to use select in order to interact in an ordered way with all of the messages that are getting received
+
+func doSomething(i time.Duration, c chan<- int, param int) {
+	time.Sleep(i)
+	c <- param
+}
+
+func main() {
+	c1 := make(chan int)
+	c2 := make(chan int)
+	d1 := 4 * time.Second
+	d2 := 2 * time.Second
+
+	go doSomething(d1, c1, 1)
+	go doSomething(d2, c2, 2)
+
+	// fmt.Println(<-c1)
+	// fmt.Println(<-c2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case channelMsg1 := <-c1:
+			fmt.Println(channelMsg1)
+
+		case channelMsg2 := <-c2:
+			fmt.Println(channelMsg2)
+
+		}
+	}
+}
